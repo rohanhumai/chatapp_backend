714207dac7e5cc5496a88361dea3b91905f1646f
Rohan Coder <sablerohan125@gmail.com>
2026-02-14 21:56:55 +0530

    Changed utils

---
+++ src/utils/constants.js
@@ -0,0 +1,20 @@
+module.exports = {
+  MESSAGE_STATUS: {
+    SENT: "sent",
+    DELIVERED: "delivered",
+    READ: "read",
+  },
+  SOCKET_EVENTS: {
+    SEND_MESSAGE: "send_message",
+    NEW_MESSAGE: "new_message",
+    MESSAGE_SENT: "message_sent",
+    MESSAGE_ERROR: "message_error",
+    TYPING_START: "typing_start",
+    TYPING_STOP: "typing_stop",
+    USER_ONLINE: "user_online",
+    USER_OFFLINE: "user_offline",
+    JOIN_CONVERSATION: "join_conversation",
+    LEAVE_CONVERSATION: "leave_conversation",
+    KEY_EXCHANGE: "key_exchange",
+  },
+};
