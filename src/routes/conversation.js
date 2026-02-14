b670d6c803b70085baa4619876f6ab0e62e3e540
Rohan Coder <sablerohan125@gmail.com>
2026-02-14 21:53:35 +0530

    Changed routes

---
+++ src/routes/conversation.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { authenticate } = require("../middleware/auth");
+const conversationController = require("../controllers/conversationController");
+
+router.get("/", authenticate, conversationController.getConversations);
+router.post("/", authenticate, conversationController.createConversation);
+router.get("/:id", authenticate, conversationController.getConversation);
+
+module.exports = router;
